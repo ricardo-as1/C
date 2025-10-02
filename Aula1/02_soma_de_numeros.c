9e0759714357a943b76993a81f5436bc6a27bd47
ricardo-as1 <ricardo@autobookr.com>
2025-10-02 02:01:08 -0300

    Aula 1 - Soma de Números: Soma dois números informados no terminal.

---
+++ Aula1/02_soma_de_numeros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()  {
+  int N1, N2, S;
+  printf("Digite o numero 1: ");
+  scanf("%d", &N1);
+  printf("Digite o numero 2: ");
+  scanf("%d", &N2);
+  S = N1 + N2;
+  printf("Resultado da soma: %d\n", S);
+  return 0;
+}
